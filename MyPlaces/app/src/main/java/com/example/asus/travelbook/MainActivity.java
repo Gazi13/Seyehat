f1c3bd4d9994048fc16611a4818839d3e267f0eb
Gazi13 <ahmetgazi03@gmail.com>
2017-09-06 10:57:18 +0300

    Seyehat Kitabı
    Seyehat Kitabı

---
+++ MyPlaces/app/src/main/java/com/example/asus/travelbook/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.asus.travelbook;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_place, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.addPlace:
+                startActivity(new Intent(MainActivity.this,MapsActivity.class).putExtra("info","new"));
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+    }
+
+
+}
